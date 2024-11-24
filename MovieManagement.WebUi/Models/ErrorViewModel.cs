5c1aceeb9f8740b8d38427d16a5ada548c0a4259
AslamNazeerShaikh <Aslam.Nazeer.Shaikh@outlook.in>
2024-11-25 02:01:12 +0530

    Added WebUi Layer

---
+++ MovieManagement.WebUi/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MovieManagement.WebUi.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
