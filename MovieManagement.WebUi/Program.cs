591278786366448f48952fcbe6ab293001ae4d4d
AslamNazeerShaikh <aslam.nazeer.shaikh@gmail.com>
2025-02-18 17:07:40 +0530

    Added Blazor SSR Project Layer.

---
+++ MovieManagement.WebUi/Program.cs
@@ -0,0 +1,42 @@
+using Microsoft.EntityFrameworkCore;
+using MovieManagement.Infrastructure.Context;
+using MovieManagement.WebUi.Components;
+
+namespace MovieManagement.WebUi;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddRazorComponents()
+            .AddInteractiveServerComponents();
+
+        builder.Services.AddDbContextFactory<AppDbContext>(options =>
+        {
+            options.UseMySQL(builder.Configuration.GetConnectionString("DefaultConnection"));
+        });
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Error");
+            // The default HSTS value is 30 days. 
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAntiforgery();
+
+        app.MapStaticAssets();
+        app.MapRazorComponents<App>()
+            .AddInteractiveServerRenderMode();
+
+        app.Run();
+    }
+}
